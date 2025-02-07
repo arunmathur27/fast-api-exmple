300d0793415d3ac70efc01223513e8dd335801c2
Arun Mathur <104275883+arunmathur27@users.noreply.github.com>
2025-02-07 07:55:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,27 +3,26 @@ from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import os
 
-
 IMAGEDIR = "images/"
 
 app = FastAPI()
+
+# Ensure the directory exists
+if not os.path.exists(IMAGEDIR):
+    os.makedirs(IMAGEDIR)
 
 # Root route
 @app.get("/")
 def root():
     return {"message": "Welcome to the FastAPI root route!"}
-    
 
 @app.post("/upload/")
 async def create_upload_file(file: UploadFile):
-    file_name = os.getcwd() + "/images/" + file.filename.replace(" ", "-")
-    #filename = 'Arunmathur'
-    #file.filename = f"{filename}.jpg"
+    file_name = os.path.join(IMAGEDIR, file.filename.replace(" ", "-"))
     contents = await file.read()
 
-    # save the file
-    with open(file_name,'wb+') as f:
+    # Save the file
+    with open(file_name, 'wb+') as f:
         f.write(contents)
 
     return {"filename": file.filename}
-
