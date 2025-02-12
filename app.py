9e64a2f278915cb8f3d1eebcf4a95a98b0d9e369
Arun Mathur <104275883+arunmathur27@users.noreply.github.com>
2025-02-12 12:33:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,24 +2,35 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.responses import FileResponse
 import os
+import uvicorn
 
 
-app = FastAPI()
+def init_app():
+    app = FastAPI(
+         title='Test FastAPI',
+         description="Fast API Test",
+         version="1.0.1")
 
-# Root route
-@app.get("/")
-def root():
-    return {"message": "Welcome to the FastAPI root route!"}
-
-@app.post("/upload/")
-async def create_upload_file(file: UploadFile):
-    file_name = os.getcwd() +"/"+ file.filename.replace(" ", "-")
-    contents = await file.read()
-
-    # save the file
-    with open(file_name,'wb+') as f:
-        f.write(contents)
-
-    return {"filename": file.filename}
+    # Root route
+    @app.get("/")
+    def root():
+        return {"message": "Welcome to the FastAPI root route!"}
 
 
+    @app.post("/upload/")
+    async def create_upload_file(file: UploadFile):
+        file_name = os.getcwd() +"/"+ file.filename.replace(" ", "-")
+        contents = await file.read()
+
+        # save the file
+        with open(file_name,'wb+') as f:
+            f.write(contents)
+        return {"filename": file.filename}
+
+    return app
+
+app = init_app()
+
+if __name__== '__main__':
+    uvicorn.run("app:app", host="localhost", port=8888, reload=True)
+    
