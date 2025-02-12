e2792830f951625cfedadf6f33f7857fd2ffd4a6
Arun Mathur <104275883+arunmathur27@users.noreply.github.com>
2025-02-12 13:06:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # upload/main.py
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI, File, UploadFile, HTTPException
 from fastapi.responses import FileResponse
 import os
 import uvicorn
+
 
 def init_app():
     app = FastAPI(
@@ -17,13 +18,19 @@ def init_app():
 
 
     @app.post("/upload/")
-    async def create_upload_file(file: UploadFile):
-        file_name = os.getcwd() +"/"+ file.filename.replace(" ", "-")
-        contents = await file.read()
+    async def create_upload_file(file: UploadFile = File(...)):
+        try:
+            file_name = os.getcwd() +"/"+ file.filename.replace(" ", "-")
+            contents = await file.read()
 
-        # save the file
-        with open(file_name,'wb+') as f:
-            f.write(contents)
+            # save the file
+            with open(file_name,'wb+') as f:
+                f.write(contents)
+        except Exception:
+            raise HTTPException(status_code=500, detail='Something went wrong')
+        finally:
+            file.file.close()
+
         return {"filename": file.filename}
 
     return app
@@ -32,4 +39,3 @@ app = init_app()
 
 if __name__== '__main__':
     uvicorn.run("app:app", host="localhost", port=8888, reload=True)
-    
